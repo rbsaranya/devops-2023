cd9ad902216098a5204dd87b9cf5fd221872e9ae
rbsaranya <96078773+rbsaranya@users.noreply.github.com>
2023-10-13 12:39:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,10 +61,12 @@ resource "aws_key_pair" "kp" {
   public_key = tls_private_key.pk.public_key_openssh
 
   provisioner "local-exec" {
-    command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem chmod 400 ./myKey.pem"
-  }
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
 }
-
+ provisioner "local-exec" {
+    command = "chmod 400 ./myKey.pem"
+}
+}
   resource "aws_instance" "web_instance" {
   ami             = var.ami_id
   instance_type   = var.inst_type
